fd4f201d7a736b4df428471cdd62cba594b44412
Davis <dpjankevics@gmail.com>
2024-02-06 16:00:41 +0200

    ckpt file setup fix?

--- src/inference.py
+++ src/inference.py
@@ -23,7 +23,7 @@ class InferenceProgram:
         self.tgt_dir = self.SAVED_MODELS_DIR / target
         # path to checkpoint
         if ckpt_path is None:
-            ckpt_path = self.tgt_dir / f"{target}.pt"
+            ckpt_path = self.tgt_dir / f"{target}.ckpt"
             if not ckpt_path.is_file():
                 raise ValueError("{ckpt_path} is missing. Please provide 'ckpt_path' explicitly.")
         self.ckpt_path = ckpt_path

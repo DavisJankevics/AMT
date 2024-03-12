6893ea5bdedba623015de3b667d9cc922afd0e5b
Davis <dpjankevics@gmail.com>
2024-03-12 22:54:17 +0200

    update lr again

--- MyAMT/src/train.py
+++ MyAMT/src/train.py
@@ -19,7 +19,7 @@ else:
     print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
 def scheduler(epoch, lr):
-    if epoch%10 != 0:
+    if epoch%10 != 0 or epoch < 50:
         return lr
     else:
         return lr * tf.math.exp(-0.1)
@@ -61,8 +61,8 @@ def train(db_location, load_model_path=None):
         optimizer = Adam(learning_rate=config.learning_rate)
         model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy', Precision(), Recall()])
 
-    # optimizer = Adam(learning_rate=config.learning_rate)
-    # model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy', Precision(), Recall()])
+    optimizer = Adam(learning_rate=config.learning_rate)
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy', Precision(), Recall()])
     train_dataset = create_tf_dataset(root_dir=db_location, split='train', sr=config.sr, hop_length=config.hop_length, n_mfcc=config.n_mfcc)
     val_dataset = create_tf_dataset(root_dir=db_location, split='validation', sr=config.sr, hop_length=config.hop_length, n_mfcc=config.n_mfcc)
     
